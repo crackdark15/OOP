1ce97f07f78cf9da5552fed0852aa1f6e564b3bd
Hanel Tonna <crackdark15@gmail.com>
2019-05-11 11:33:00 +0700

    Initial Commit

---
+++ emotions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emotions
+//
+//  Created by Hansel Tonna on 11/05/19.
+//  Copyright © 2019 Hansel Tonna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
